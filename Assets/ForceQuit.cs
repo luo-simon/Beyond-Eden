760be7d4ef225467eb976a75fc018de417fe8213
Simon <47718189+luo-simon@users.noreply.github.com>
2020-01-04 17:59:43 +0000

    Add files via upload

---
+++ Assets/ForceQuit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceQuit : MonoBehaviour
+{
+    public DialogueManager dialogueManager;
+
+    public bool initiated = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueManager = GetComponent<DialogueManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dialogueManager.isPlaying == true)
+        {
+            initiated = true;
+        }
+
+        if (initiated)
+        {
+            if (dialogueManager.isPlaying == false) {
+                Application.Quit();
+                Debug.Log("QUIT");
+            }
+        }
+    }
+}
